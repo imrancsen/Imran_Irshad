6546939e8d30b8385e495fd00ab40987b9a27f08
imrancsen <97589134+imrancsen@users.noreply.github.com>
2022-02-16 10:26:03 +0200

    js

---
+++ JS/Imran.JS.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h2>This is in body of html and outside JS,  Helloo!</h2>
+    <script>
+// <!-- alert boxes -->
+    window.alert("alert boxes are quite annoying, dnt use many if not necessary");
+    document.write ("this is in JS script, Hello")
+        </script>
+    <script src="ImranJS.js"></script>
+    <button type="button" onclick="Imranjsfunction()">Click button</button>
+<script>
+    docu
+    </script>
+
+    <noscript>
+        install or update or enable or check
+</noscript>
+</body>
+</html>
